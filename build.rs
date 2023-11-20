44d730a96d19f68ca3a5c204ecff8c64a3d2c0a3
Antoine Gaudreau Simard <antoineg.simard@gmail.com>
2023-11-20 08:58:04 -0500

    Fix incorrect Icon Id crashing the app upon startup

--- build.rs
+++ build.rs
@@ -2,6 +2,6 @@ use winres::WindowsResource;
 
 fn main() {
     let mut res = WindowsResource::new();
-    res.set_icon("microsoft-teams.ico");
+    res.set_icon_with_id("microsoft-teams.ico", "default-icon");
     res.compile().unwrap();
 }
